edf78695f2f517be58beee6cda5b2741d9b59f19
Antonio Piras <antopiras89@gmail.com>
2021-03-05 17:09:26 +0100

    Move to access token base authentication for Twitter

--- src/_data/tweets.js
+++ src/_data/tweets.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
 
 const fetchTweets = async () => {
