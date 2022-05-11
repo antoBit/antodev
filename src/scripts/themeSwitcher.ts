572e28aa59b6287e709d213b1079a6266142c235
Antonio Piras <antopiras89@gmail.com>
2022-05-11 13:23:03 +0200

    Add theme switcher script

---
+++ src/scripts/themeSwitcher.ts
@@ -0,0 +1,24 @@
+function loadTheme() {
+    const prefersDarkTheme = localStorage.getItem('darkTheme') !== null
+    document.body.classList.toggle('darktheme', prefersDarkTheme)   
+}
+
+function switchTheme() {
+    const prefersDarkTheme = localStorage.getItem('darkTheme') !== null
+
+    if (prefersDarkTheme) {
+        localStorage.removeItem('darkTheme')
+    } else {
+        localStorage.setItem('darkTheme', 'enabled')
+    }
+
+    document.body.classList.toggle('darktheme', prefersDarkTheme)   
+}
+
+const themeSwitcher = document.getElementById('theme-switcher')
+
+if (themeSwitcher) {
+themeSwitcher.addEventListener('click', switchTheme)
+}
+
+window.addEventListener('load', (loadTheme))
